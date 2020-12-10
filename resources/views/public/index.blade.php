c1338c49274af7a1ad00d7047fbb55cbdb1b726e
Juan Jose Jolon <jjolong@miumg.edu.gt>
2020-12-10 14:38:45 -0600

    change

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -4,7 +4,7 @@
     <div >
         <!-- <video-component></video-component> -->
         <div class="video2" id="video2">
-            <iframe src="https://video.ibm.com/embed/24017916?autoplay=true&showtitle=false" style="border: 0;" webkitallowfullscreen allowfullscreen frameborder="no" width="100%" height="100%" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://video.ibm.com/embed/24017916?autoplay=true&showtitle=false" style="border: 0;" webkitallowfullscreen allowfullscreen frameborder="no" width="100%" height="100%" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
 
