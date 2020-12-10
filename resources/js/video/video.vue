9848bac98c20fa4feb4214f34432dcd7b5c7a8a3
Juan Jose Jolon <jjolong@miumg.edu.gt>
2020-12-10 14:15:21 -0600

    Initial commit

---
+++ resources/js/video/video.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="video1">
+
+    
+      <!-- <vue-core-video-player controls :src="videoSource"></vue-core-video-player> -->
+    <video-player  
+                 class="video-player-box"
+                 ref="videoPlayer"
+                 reset
+                 :options="playerOptions"
+                 :playsinline="true"
+                 @ended="onPlayerEnded($event)"
+                 customEventName="customstatechangedeventname"
+                 @ready="playerReadied">
+  </video-player>
+  </div>
+</template>
+<script>
+
+  import VuePlaylist from 'vue-playlist';
+  // Similarly, you can also introduce the plugin resource pack you want to use within the component
+  // import 'some-videojs-plugin'
+  export default {
+    data() {
+      return {
+        videos: {
+          type: "video/mp4",
+          src: './video/1.mp4'
+        },
+        videoSource :[
+            {
+              src: 'https://media.vued.vanthink.cn/sparkle_your_name_am360p.mp4',
+              resolution: '360p',
+            }, {
+              src: 'https://media.vued.vanthink.cn/sparkle_your_name_am720p.mp4',
+              resolution: '720p',
+            }, {
+              src: 'https://media.vued.vanthink.cn/y2mate.com%20-%20sparkle_your_name_amv_K_7To_y9IAM_1080p.mp4',
+              resolution: '1080p'
+            }
+          ],
+
+        playerOptions: {
+          // videojs options
+          
+          fluid: true,
+          language: 'es',
+          // autoplay: true,
+          // loop:true,
+          playbackRates: [0.7, 1.0, 1.5, 2.0],
+          sources: [
+            {
+              type: "video/mp4",
+              src: './video/1.mp4'
+            }
+          ],
+          sources: [
+            {
+              type: "video/mp4",
+              src: './video/2.mp4'
+            }
+          ],
+          
+        }
+      }
+    },
+    mounted() {
+      
+      console.log('this is current player instance object', this.player)
+    },
+    computed: {
+      player() {
+        return this.$refs.videoPlayer.player
+      }
+    },
+    methods: {
+      
+      // listen event
+      onPlayerPlay(player) {
+        // console.log('player play!', player)
+      },
+      onPlayerEnded(player) {
+        console.log('player end!', player)
+      },
+      // ...player event
+
+      // or listen state event
+      playerStateChanged(playerCurrentState) {
+        // console.log('player current update state', playerCurrentState)
+      },
+
+      // player is ready
+      playerReadied(player) {
+        console.log('the player is readied', player)
+        // you can use it to do something...
+        // player.[methods]
+      }
+    }
+  }
+</script>
---
+++ resources/views/public/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('content')
+
+    <div >
+        <video-component></video-component>
+        <div class="video2">
+        <iframe src="https://video.ibm.com/embed/24017916?autoplay=true&showtitle=false" style="border: 0;" webkitallowfullscreen allowfullscreen frameborder="no" width="720" height="405" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </div>
+@endsection
